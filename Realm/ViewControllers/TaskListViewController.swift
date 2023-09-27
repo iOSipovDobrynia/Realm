2a25fec2a758ba0e4a136b380659b5f0e84843e5
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-09-27 20:37:47 +0300

    setup project

---
+++ Realm/ViewControllers/TaskListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TaskListViewController.swift
+//  Realm
+//
+//  Created by Goodwasp on 27.09.2023.
+//
+
+import UIKit
+
+final class TaskListViewController: UIViewController {
+
+    // MARK: - View's life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+}
+
+// MARK: - View Setting
+private extension TaskListViewController {
+    func setupView() {
+        view.backgroundColor = .cyan
+        
+        setupLayout()
+    }
+}
+
+// MARK: - Settings
+private extension TaskListViewController {
+    
+}
+
+// MARK: - Layout
+private extension TaskListViewController {
+    func setupLayout() {
+        
+    }
+}
