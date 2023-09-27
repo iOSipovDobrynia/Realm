7eae8364d1cf4d6367afc332e4bc6b105c3b0c60
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-09-27 21:53:22 +0300

    accessoryView issue

--- Realm/ViewControllers/TaskListViewController.swift
+++ Realm/ViewControllers/TaskListViewController.swift
@@ -11,6 +11,7 @@ final class TaskListViewController: UITableViewController {
 
     // MARK: - Private properties
     private let cellID = "task"
+    private let taskList = ["List"]
     
     // MARK: - View's life cycle
     override func viewDidLoad() {
@@ -61,14 +62,38 @@ private extension TaskListViewController {
 // MARK: - UITableViewDataSource
 extension TaskListViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+//        
+//        let task = taskList[indexPath.row]
+//        var content = cell.defaultContentConfiguration()
+//        content.text = task
+//        let label = UILabel(frame: .zero)
+//        label.text = "1"
+//        cell.accessoryView = label
+//        cell.contentConfiguration = content
+//
+//        return cell
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
         
-        
-        
+        let task = taskList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = task
+
+        // Создайте метку и установите ей текст
+        let label = UILabel(frame: .zero)
+        label.text = "1"
+
+        // Установите метку в качестве accessoryView
+        cell.accessoryView = label
+
+        // Установите конфигурацию контента
+        cell.contentConfiguration = content
+
         return cell
     }
 }
