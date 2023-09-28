848933d1b00a9f869abdbb59b4ed6c813491fb56
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-09-28 23:48:49 +0300

    detail issue

--- Realm/ViewControllers/TaskListViewController.swift
+++ Realm/ViewControllers/TaskListViewController.swift
@@ -66,32 +66,23 @@ extension TaskListViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
-//        
-//        let task = taskList[indexPath.row]
-//        var content = cell.defaultContentConfiguration()
-//        content.text = task
-//        let label = UILabel(frame: .zero)
-//        label.text = "1"
-//        cell.accessoryView = label
-//        cell.contentConfiguration = content
-//
-//        return cell
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
         
         let task = taskList[indexPath.row]
+        
         var content = cell.defaultContentConfiguration()
         content.text = task
+        cell.accessoryType = .checkmark
+        
+        // first create UIImageView
+        var imageView : UIImageView
+        imageView  = UIImageView(frame:CGRect(x: 0, y: 0, width: 20, height: 20))
+        imageView.image = UIImage(systemName: "person")
+        imageView.contentMode = .scaleAspectFit
 
-        // Создайте метку и установите ей текст
-        let label = UILabel(frame: .zero)
-        label.text = "1"
-
-        // Установите метку в качестве accessoryView
-        cell.accessoryView = label
-
-        // Установите конфигурацию контента
+        // then set it as cellAccessoryType
+        cell.accessoryView = imageView
+        
         cell.contentConfiguration = content
 
         return cell
